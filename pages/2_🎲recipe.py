c0c3e28e1af3d086c5e23230eb47d2e6c67f6b55
Dandi Septiandi <107525268+duaribusatu@users.noreply.github.com>
2024-03-24 14:10:57 +0700

    Update 2_🎲recipe.py

--- pages/2_🎲recipe.py
+++ pages/2_🎲recipe.py
@@ -10,10 +10,8 @@ from scripts.auth import azure_ad_auth
 st.title("Analyze ur Recipe")
 
 # Auth
-auth_result = azure_ad_auth('home', True)
-if auth_result:
-    email, username, full_name, uid = auth_result
-    st.text(f'email, username, full_name, uid: {(email, username, full_name, uid)}')
+email, username, full_name, uid = azure_ad_auth('home', True)
+st.text(f'email, username, full_name, uid: {(email, username, full_name, uid)}')
 
 # Search Engine
 service_endpoint = 'https://team4search.search.windows.net'
